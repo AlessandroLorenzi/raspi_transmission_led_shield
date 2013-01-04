6e04ef67e6db57144bc55fc153974b9522d3fbe7
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2013-01-04 15:36:14 +0100

    used transmissionrpc python class

--- raspi_transmission_led_shield.py
+++ raspi_transmission_led_shield.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*- 
 import RPi.GPIO as GPIO, time
-import subprocess
+import transmissionrpc
 
 def transmission_is_up():
-	p = subprocess.Popen("/usr/bin/ps aux| grep transmission-daemon | grep -v grep | wc -l", stdout=subprocess.PIPE ,shell=True)
-	out =  int(p.stdout.read())
-	if out == 1:
-	        GPIO.output(24, False) 
-                check_download()
-
-	else:
-	        GPIO.output(18, False)  
-	        GPIO.output(23, False)  
-	        GPIO.output(24, True) 
+	try:
+		server = transmissionrpc.Client("localhost")
+	except:
+		GPIO.output(18, False)  
+		GPIO.output(23, False)  
+		GPIO.output(24, True) 
+		return False
+	
+	GPIO.output(24, False) 
+	return True
 
 def check_download():
-	p = subprocess.Popen("transmission-remote localhost  --list | wc -l", stdout=subprocess.PIPE ,shell=True)
-	active =  int(p.stdout.read())-2
+	server = transmissionrpc.Client("localhost")
+	downloading = 0
+	seeding = 0
+	for torrent in server.list():
+		if server.list()[torrent].status == "downloading":
+			downloading += 1
+		if server.list()[torrent].status == "seeding":
+			seeding += 1
 
-	p = subprocess.Popen("transmission-remote localhost  --list |grep \"100%\" |  wc -l", stdout=subprocess.PIPE ,shell=True)
-	done =  int(p.stdout.read())
-	if active > 0:
+	if downloading > 0:
 		GPIO.output(23, True)  
 	else:
 		GPIO.output(23, False)  
 	
-	if done > 0:
+	if seeding > 0:
 		GPIO.output(18, True)  
 	else:
 		GPIO.output(18, False)  
@@ -40,7 +44,9 @@ GPIO.setup(23, GPIO.OUT) # definiamo che il la GPIO18 (pin12) è un uscita
 GPIO.setup(24, GPIO.OUT) # definiamo che il la GPIO18 (pin12) è un uscita
 
 while 1:
-	transmission_is_up()
+	if transmission_is_up():
+		check_download()
+
 	time.sleep(1)
 
 
