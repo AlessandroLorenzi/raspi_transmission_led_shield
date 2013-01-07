951826fcb538c21052c3f5e8049918a1fef98c99
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2013-01-08 00:27:02 +0100

    addedd button to start/stop transmission

--- raspi_transmission_led_shield.py
+++ raspi_transmission_led_shield.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import RPi.GPIO as GPIO, time
 import transmissionrpc
+import subprocess
 
 def transmission_is_up():
 	try:
@@ -15,15 +17,16 @@ def transmission_is_up():
 	return True
 
 def check_download():
-	server = transmissionrpc.Client("localhost")
+
 	downloading = 0
 	seeding = 0
+
+	server = transmissionrpc.Client("localhost")
 	for torrent in server.list():
 		if server.list()[torrent].status == "downloading":
 			downloading += 1
 		if server.list()[torrent].status == "seeding":
 			seeding += 1
-
 	if downloading > 0:
 		GPIO.output(23, True)  
 	else:
@@ -35,28 +38,62 @@ def check_download():
 		GPIO.output(18, False)  
 		
 
-		
+def transmission_on_off(oldstatus):
+	# Read if button is not pressed. 
+	# i wonder why if not pressed return value is a random value (true/false)
+	# while if is pressed is true
+
+	count = 0
+	while count < 5000:
+		if GPIO.input(25) == True:
+			return False
+		count +=1
+	
+	if oldstatus==True:
+		return True
+
+	status = transmission_is_up()
+	GPIO.output(18, True)  
+	GPIO.output(23, True)  
+	GPIO.output(24, True) 
+	print "slipping"
+
+	if status:
+		print ("killing transmission")
+		subprocess.Popen("killall transmission-daemon", shell=True)
+		subprocess.Popen("umount /home/alorenzi/Transmission", shell=True)
+	else:
+		print ("starting transmission")
+		subprocess.Popen("mount -L transmission ", shell=True)
+		subprocess.Popen("su alorenzi -c transmission-daemon", shell=True)
+	
+	return True
+
 	
 
 GPIO.setmode(GPIO.BCM)  
 GPIO.setup(18, GPIO.OUT) # definiamo che il la GPIO18 (pin12) è un uscita
 GPIO.setup(23, GPIO.OUT) # definiamo che il la GPIO18 (pin12) è un uscita
 GPIO.setup(24, GPIO.OUT) # definiamo che il la GPIO18 (pin12) è un uscita
+GPIO.setup(25, GPIO.IN) # definiamo che il GPIO17 è un ingresso
 
+r = 0
+
+
+oldstatus = False
 while 1:
-	if transmission_is_up():
-		check_download()
+	if r == 0:
+		r = 1000
+		if transmission_is_up():
+			check_download()
+	r -= 1
+	
+	oldstatus=transmission_on_off(oldstatus)
+	
+	if oldstatus:
+		time.sleep(1)
+		r = 0
 
-	time.sleep(1)
+	time.sleep(0.1)
 
 
-def round(): 
-	GPIO.output(24, False)  # GPIO a 1 (HIGH), led accesso
-	GPIO.output(18, True)  # GPIO a 1 (HIGH), led accesso
-	time.sleep(1)          # attendi 1 secondo
-	GPIO.output(18, False) # GPIO a 0 (LOW), led spento
-	GPIO.output(23, True)  # GPIO a 1 (HIGH), led accesso
-	time.sleep(1)          # attendi 1 secondo e ricomincia
-	GPIO.output(23, False)  # GPIO a 1 (HIGH), led accesso
-	GPIO.output(24, True)  # GPIO a 1 (HIGH), led accesso
-	time.sleep(1)          # attendi 1 secondo
